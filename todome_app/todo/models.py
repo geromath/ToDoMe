688ae4bb75160926f33204caeaaeb6869c3b3326
Mathias Maagerø Svendsen <mathiams@stud.ntnu.no>
2017-02-20 14:33:14 +0100

    RESTART

--- todome_app/todo/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Todo (models.Model):
-    todo_text = models.CharField(max_length = 200)
-    date = models.DateTimeField('finish date')
--- todome_app/todome_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-
-urlpatterns = [
-    url(r'^', include('todo.urls')),
-    url(r'^admin/', admin.site.urls),
-]
