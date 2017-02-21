dcd2796d184c784644b9d7eb68d31da2d62a9669
Erikkvo <Erikkvo@hotmail.com>
2017-02-21 11:51:53 +0100

    Minor changes

--- root/todolist/views.py
+++ root/todolist/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    all_tasks = Task.objects.all()
+    context = {
+        'all_tasks': all_tasks
+    }
+    return render(request, 'todolist/index.html', context)
+
+
+
