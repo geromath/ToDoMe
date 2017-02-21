c0d8df33a868e418c246476f14ddc61a65edfdca
Marius Hornslien <mariushornslien@dhcp-10-24-16-107.wlan.ntnu.no>
2017-02-21 11:47:37 +0100

    opprettet forms.py, opprettet urls.py, lagt til elementer i views.py, opprettet registration_form.html. Jobber med brukerregistrering.

---
+++ root/todolist/urls.py
@@ -0,0 +1,11 @@
+#denne maa inkluderes i root-url-filen
+from django.conf.urls import url
+from . import views
+
+app_name = 'todolist'
+
+urlpatterns = [
+
+    url(r'register/^$', views.UserFormView.as_view(), name='register'),
+
+]
--- root/todolist/views.py
+++ root/todolist/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
+#importerer forms-klassen vi lagde
 
-# Create your views here.
+class UserFormView(View):
+    form_class = UserForm #blueprint til det vi skal bruke
+
+    template_name = 'todolist/registration_form.html' #maa lages!
+
+    #display blank form
+    def get(self, request): #innebygd funksjon for get-requests. f.eks. laste inn skjema som skal fylles ut
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    #legge til brukeren i databasen
+    def post(self, request): #innebygd funksjon for post-requests
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            #cleaned normalized data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+
+            user.set_password(password)
+            user.save() #lagrer brukeren i databasen
+
+            #Returns User object if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active: #dersom de ikke er banned/utestengt etc
+
+                    login(request, user)
+
+                    return redirect('todolist:index') #maa ogsaa lages! Sender brukeren til startsiden etter login
+
+        return render(request, self.template_name, {'form': form}) #gir skjemaet paa nytt om noe gikk galt
+
+
+
+
+
+
+
