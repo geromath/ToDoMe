238b5ce91bcd9d4f98c0cb2705da601193259717
Mathias Maagerø Svendsen <mathiams@stud.ntnu.no>
2017-03-06 14:50:29 +0100

    did some changes to model

--- root/todolist/models.py
+++ root/todolist/models.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse, reverse_lazy
 class Task(models.Model):
     task_text = models.CharField(max_length = 150)
     description = models.CharField(max_length = 400)
-    due_date = models.DateTimeField(default=None, null=True)
+    due_date = models.DateTimeField(default=None, null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse('todolist:index')
