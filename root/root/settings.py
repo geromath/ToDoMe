8c9405e24e3cfd76934ee5b144550bfdfaf76712
Marius Hornslien <mariushornslien@Mariuss-MacBook-Pro-2.local>
2017-03-09 21:54:19 +0100

    fjernet url-lenke og settings-elementer som ikke skulle vare der

--- root/root/settings.py
+++ root/root/settings.py
@@ -38,10 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quiz',
-    'multichoice',
-    'true_false',
-    'essay',
+
 ]
 
 MIDDLEWARE = [
