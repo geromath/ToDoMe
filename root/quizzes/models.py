ff75bd062c4ee75ca1d6b85b6a00b9bc86ed0d5c
Marius Hornslien <mariushornslien@Mariuss-MacBook-Pro-2.local>
2017-03-09 23:08:51 +0100

    lagd 3 klasser

--- root/quizzes/models.py
+++ root/quizzes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+#dette skal vaere en samling av flere spoersmaal
+class Questionnaire(models.Model):
+    name = models.CharField(max_length=100, verbose_name=u'Questionnaire name')
+    slug = models.SlugField()
+
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=1000)
+    questionnaire = models.ForeignKey(Questionnaire, related_name='questions', on_delete=models.CASCADE,
+                                      default="") #relasjon til questionnaire-klassen
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE) #relasjon til question-klassen
+    answer_text = models.CharField(max_length=200)
+    is_valid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer_text
+
+
+
--- root/root/settings.py
+++ root/root/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'todolist.apps.TodolistConfig',
+    'quizzes.apps.QuizzesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
