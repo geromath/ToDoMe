d94a0732581eca81347ae6ed4806c4488f1b8d38
Marius Hornslien <mariushornslien@dhcp-10-24-17-243.wlan.ntnu.no>
2017-03-10 15:56:51 +0100

    gjort ferdig models

--- root/quizzes/models.py
+++ root/quizzes/models.py
@@ -1,14 +1,12 @@
-from django.db import models
-from model_utils.managers import InheritanceManager
-from django.utils.translation import gettext as _
-
-
 import re
 import json
 
+from model_utils.managers import InheritanceManager
+from django.utils.translation import gettext as _
+
 from django.db import models
 from django.core.exceptions import ValidationError, ImproperlyConfigured
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, validate_comma_separated_integer_list
 from django.utils.translation import ugettext as _
 from django.utils.timezone import now
 from django.utils.encoding import python_2_unicode_compatible
@@ -182,6 +180,357 @@ class Quiz(models.Model):
         return str(self.id) + "_data"
 
 
+class ProgressManager(models.Manager):
+
+    def new_progress(self, user):
+        new_progress = self.create(user=user,
+                                   score="")
+        new_progress.save()
+        return new_progress
+
+
+class Progress(models.Model):
+    """
+    Progress is used to track an individual signed in users score on different
+    quiz's and categories
+    Data stored in csv using the format:
+        category, score, possible, category, score, possible, ...
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name=_("User"))
+
+    score = models.CharField(validators=[validate_comma_separated_integer_list], max_length=1024,
+                                              verbose_name=_("Score"))
+
+    objects = ProgressManager()
+
+    class Meta:
+        verbose_name = _("User Progress")
+        verbose_name_plural = _("User progress records")
+
+    @property
+    def list_all_cat_scores(self):
+        """
+        Returns a dict in which the key is the category name and the item is
+        a list of three integers.
+        The first is the number of questions correct,
+        the second is the possible best score,
+        the third is the percentage correct.
+        The dict will have one key for every category that you have defined
+        """
+        score_before = self.score
+        output = {}
+
+        for cat in Category.objects.all():
+            to_find = re.escape(cat.category) + r",(\d+),(\d+),"
+            #  group 1 is score, group 2 is highest possible
+
+            match = re.search(to_find, self.score, re.IGNORECASE)
+
+            if match:
+                score = int(match.group(1))
+                possible = int(match.group(2))
+
+                try:
+                    percent = int(round((float(score) / float(possible))
+                                        * 100))
+                except:
+                    percent = 0
+
+                output[cat.category] = [score, possible, percent]
+
+            else:  # if category has not been added yet, add it.
+                self.score += cat.category + ",0,0,"
+                output[cat.category] = [0, 0]
+
+        if len(self.score) > len(score_before):
+            # If a new category has been added, save changes.
+            self.save()
+
+        return output
+
+    def update_score(self, question, score_to_add=0, possible_to_add=0):
+        """
+        Pass in question object, amount to increase score
+        and max possible.
+        Does not return anything.
+        """
+        category_test = Category.objects.filter(category=question.category)\
+                                        .exists()
+
+        if any([item is False for item in [category_test,
+                                           score_to_add,
+                                           possible_to_add,
+                                           isinstance(score_to_add, int),
+                                           isinstance(possible_to_add, int)]]):
+            return _("error"), _("category does not exist or invalid score")
+
+        to_find = re.escape(str(question.category)) +\
+            r",(?P<score>\d+),(?P<possible>\d+),"
+
+        match = re.search(to_find, self.score, re.IGNORECASE)
+
+        if match:
+            updated_score = int(match.group('score')) + abs(score_to_add)
+            updated_possible = int(match.group('possible')) +\
+                abs(possible_to_add)
+
+            new_score = ",".join(
+                [
+                    str(question.category),
+                    str(updated_score),
+                    str(updated_possible), ""
+                ])
+
+            # swap old score for the new one
+            self.score = self.score.replace(match.group(), new_score)
+            self.save()
+
+        else:
+            #  if not present but existing, add with the points passed in
+            self.score += ",".join(
+                [
+                    str(question.category),
+                    str(score_to_add),
+                    str(possible_to_add),
+                    ""
+                ])
+            self.save()
+
+    def show_exams(self):
+        """
+        Finds the previous quizzes marked as 'exam papers'.
+        Returns a queryset of complete exams.
+        """
+        return Sitting.objects.filter(user=self.user, complete=True)
+
+
+class SittingManager(models.Manager):
+
+    def new_sitting(self, user, quiz):
+        if quiz.random_order is True:
+            question_set = quiz.question_set.all() \
+                                            .select_subclasses() \
+                                            .order_by('?')
+        else:
+            question_set = quiz.question_set.all() \
+                                            .select_subclasses()
+
+        question_set = [item.id for item in question_set]
+
+        if len(question_set) == 0:
+            raise ImproperlyConfigured('Question set of the quiz is empty. '
+                                       'Please configure questions properly')
+
+        if quiz.max_questions and quiz.max_questions < len(question_set):
+            question_set = question_set[:quiz.max_questions]
+
+        questions = ",".join(map(str, question_set)) + ","
+
+        new_sitting = self.create(user=user,
+                                  quiz=quiz,
+                                  question_order=questions,
+                                  question_list=questions,
+                                  incorrect_questions="",
+                                  current_score=0,
+                                  complete=False,
+                                  user_answers='{}')
+        return new_sitting
+
+    def user_sitting(self, user, quiz):
+        if quiz.single_attempt is True and self.filter(user=user,
+                                                       quiz=quiz,
+                                                       complete=True)\
+                                               .exists():
+            return False
+
+        try:
+            sitting = self.get(user=user, quiz=quiz, complete=False)
+        except Sitting.DoesNotExist:
+            sitting = self.new_sitting(user, quiz)
+        except Sitting.MultipleObjectsReturned:
+            sitting = self.filter(user=user, quiz=quiz, complete=False)[0]
+        return sitting
+
+
+class Sitting(models.Model):
+    """
+    Used to store the progress of logged in users sitting a quiz.
+    Replaces the session system used by anon users.
+    Question_order is a list of integer pks of all the questions in the
+    quiz, in order.
+    Question_list is a list of integers which represent id's of
+    the unanswered questions in csv format.
+    Incorrect_questions is a list in the same format.
+    Sitting deleted when quiz finished unless quiz.exam_paper is true.
+    User_answers is a json object in which the question PK is stored
+    with the answer the user gave.
+    """
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("User"))
+
+    quiz = models.ForeignKey(Quiz, verbose_name=_("Quiz"))
+
+
+    question_order = models.CharField(validators=[validate_comma_separated_integer_list], max_length=1024,
+                                      verbose_name=_("Question Order"))
+
+    question_list = models.CharField(validators=[validate_comma_separated_integer_list],
+        max_length=1024, verbose_name=_("Question List"))
+
+    incorrect_questions = models.CharField(validators=[validate_comma_separated_integer_list],
+        max_length=1024, blank=True, verbose_name=_("Incorrect questions"))
+
+    current_score = models.IntegerField(verbose_name=_("Current Score"))
+
+    complete = models.BooleanField(default=False, blank=False,
+                                   verbose_name=_("Complete"))
+
+    user_answers = models.TextField(blank=True, default='{}',
+                                    verbose_name=_("User Answers"))
+
+    start = models.DateTimeField(auto_now_add=True,
+                                 verbose_name=_("Start"))
+
+    end = models.DateTimeField(null=True, blank=True, verbose_name=_("End"))
+
+    objects = SittingManager()
+
+    class Meta:
+        permissions = (("view_sittings", _("Can see completed exams.")),)
+
+    def get_first_question(self):
+        """
+        Returns the next question.
+        If no question is found, returns False
+        Does NOT remove the question from the front of the list.
+        """
+        if not self.question_list:
+            return False
+
+        first, _ = self.question_list.split(',', 1)
+        question_id = int(first)
+        return Question.objects.get_subclass(id=question_id)
+
+    def remove_first_question(self):
+        if not self.question_list:
+            return
+
+        _, others = self.question_list.split(',', 1)
+        self.question_list = others
+        self.save()
+
+    def add_to_score(self, points):
+        self.current_score += int(points)
+        self.save()
+
+    @property
+    def get_current_score(self):
+        return self.current_score
+
+    def _question_ids(self):
+        return [int(n) for n in self.question_order.split(',') if n]
+
+    @property
+    def get_percent_correct(self):
+        dividend = float(self.current_score)
+        divisor = len(self._question_ids())
+        if divisor < 1:
+            return 0            # prevent divide by zero error
+
+        if dividend > divisor:
+            return 100
+
+        correct = int(round((dividend / divisor) * 100))
+
+        if correct >= 1:
+            return correct
+        else:
+            return 0
+
+    def mark_quiz_complete(self):
+        self.complete = True
+        self.end = now()
+        self.save()
+
+    def add_incorrect_question(self, question):
+        """
+        Adds uid of incorrect question to the list.
+        The question object must be passed in.
+        """
+        if len(self.incorrect_questions) > 0:
+            self.incorrect_questions += ','
+        self.incorrect_questions += str(question.id) + ","
+        if self.complete:
+            self.add_to_score(-1)
+        self.save()
+
+    @property
+    def get_incorrect_questions(self):
+        """
+        Returns a list of non empty integers, representing the pk of
+        questions
+        """
+        return [int(q) for q in self.incorrect_questions.split(',') if q]
+
+    def remove_incorrect_question(self, question):
+        current = self.get_incorrect_questions
+        current.remove(question.id)
+        self.incorrect_questions = ','.join(map(str, current))
+        self.add_to_score(1)
+        self.save()
+
+    @property
+    def check_if_passed(self):
+        return self.get_percent_correct >= self.quiz.pass_mark
+
+    @property
+    def result_message(self):
+        if self.check_if_passed:
+            return self.quiz.success_text
+        else:
+            return self.quiz.fail_text
+
+    def add_user_answer(self, question, guess):
+        current = json.loads(self.user_answers)
+        current[question.id] = guess
+        self.user_answers = json.dumps(current)
+        self.save()
+
+    def get_questions(self, with_answers=False):
+        question_ids = self._question_ids()
+        questions = sorted(
+            self.quiz.question_set.filter(id__in=question_ids)
+                                  .select_subclasses(),
+            key=lambda q: question_ids.index(q.id))
+
+        if with_answers:
+            user_answers = json.loads(self.user_answers)
+            for question in questions:
+                question.user_answer = user_answers[str(question.id)]
+
+        return questions
+
+    @property
+    def questions_with_user_answers(self):
+        return {
+            q: q.user_answer for q in self.get_questions(with_answers=True)
+        }
+
+    @property
+    def get_max_score(self):
+        return len(self._question_ids())
+
+    def progress(self):
+        """
+        Returns the number of questions answered so far and the total number of
+        questions.
+        """
+        answered = len(json.loads(self.user_answers))
+        total = self.get_max_score
+        return answered, total
+
+
+
 
 class Question(models.Model):
     # relasjon til quiz-klassen
@@ -233,6 +582,10 @@ class Question(models.Model):
     def __str__(self):
         return self.content
 
+
+
+#usikker paa om answer-klassen trenger aa vaere med! Ikke med i TomWalkerQuizen
+"""
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE) #relasjon til question-klassen
     answer_text = models.CharField(max_length=200)
@@ -240,3 +593,5 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.answer_text
+
+"""
