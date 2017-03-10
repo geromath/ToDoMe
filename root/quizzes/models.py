f55e8f4d91a45492c5f8343c8387d0745cede7fb
Marius Hornslien <mariushornslien@dhcp-10-24-17-243.wlan.ntnu.no>
2017-03-10 14:58:19 +0100

    lagt til category

--- root/quizzes/models.py
+++ root/quizzes/models.py
@@ -1,24 +1,134 @@
 from django.db import models
+from model_utils.managers import InheritanceManager
+from django.utils.translation import gettext as _
+
+
+import re
+import json
+
+from django.db import models
+from django.core.exceptions import ValidationError, ImproperlyConfigured
+from django.core.validators import MaxValueValidator
+from django.utils.translation import ugettext as _
+from django.utils.timezone import now
+from django.utils.encoding import python_2_unicode_compatible
+from django.conf import settings
 
 
 #https://github.com/tomwalker/django_quiz/blob/master/quiz/models.py
 #se denne for inspirasjon
 
 #dette skal vaere en samling av flere spoersmaal
-class Questionnaire(models.Model):
-    name = models.CharField(max_length=100, verbose_name=u'Questionnaire name')
+
+
+
+
+class CategoryManager(models.Manager):
+
+    def new_category(self, category):
+        new_category = self.create(category=re.sub('\s+', '-', category)
+                                   .lower())
+
+        new_category.save()
+        return new_category
+
+
+@python_2_unicode_compatible
+class Category(models.Model):
+
+    category = models.CharField(
+        verbose_name=_("Category"),
+        max_length=250, blank=True,
+        unique=True, null=True)
+
+    objects = CategoryManager()
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.category
+
+
+@python_2_unicode_compatible
+class SubCategory(models.Model):
+
+    sub_category = models.CharField(
+        verbose_name=_("Sub-Category"),
+        max_length=250, blank=True, null=True)
+
+    category = models.ForeignKey(
+        Category, null=True, blank=True,
+        verbose_name=_("Category"))
+
+    objects = CategoryManager()
+
+    class Meta:
+        verbose_name = _("Sub-Category")
+        verbose_name_plural = _("Sub-Categories")
+
+    def __str__(self):
+        return self.sub_category + " (" + self.category.category + ")"
+
+
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u'Title')
     slug = models.SlugField()
 
     def __str__(self):
         return self.name
 
+
 class Question(models.Model):
-    question_text = models.CharField(max_length=1000)
-    questionnaire = models.ForeignKey(Questionnaire, related_name='questions', on_delete=models.CASCADE,
-                                      default="") #relasjon til questionnaire-klassen
+    # relasjon til quiz-klassen
+    """
+    Base class for all question types.
+    Shared properties placed here.
+    """
+
+    quiz = models.ManyToManyField(Quiz,
+                                  verbose_name=_("Quiz"),
+                                  blank=True)
+
+    category = models.ForeignKey(Category,
+                                 verbose_name=_("Category"),
+                                 blank=True,
+                                 null=True)
+
+    sub_category = models.ForeignKey(SubCategory,
+                                     verbose_name=_("Sub-Category"),
+                                     blank=True,
+                                     null=True)
+
+    figure = models.ImageField(upload_to='uploads/%Y/%m/%d',
+                               blank=True,
+                               null=True,
+                               verbose_name=_("Figure"))
+
+    content = models.CharField(max_length=1000,
+                               blank=False,
+                               help_text=_("Enter the question text that "
+                                           "you want displayed"),
+                               verbose_name=_('Question'))
+
+    explanation = models.TextField(max_length=2000,
+                                   blank=True,
+                                   help_text=_("Explanation to be shown "
+                                               "after the question has "
+                                               "been answered."),
+                                   verbose_name=_('Explanation'))
+
+    objects = InheritanceManager()
+
+    class Meta:
+        verbose_name = _("Question")
+        verbose_name_plural = _("Questions")
+        ordering = ['category']
 
     def __str__(self):
-        return self.question_text
+        return self.content
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE) #relasjon til question-klassen
@@ -27,6 +137,3 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.answer_text
-
-
-
