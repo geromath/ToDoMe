dbce2f5a2778383efa2d514add70381efb1833a6
Marius Hornslien <mariushornslien@dhcp-10-24-17-243.wlan.ntnu.no>
2017-03-13 13:35:48 +0100

    skrevet mine forste 2 tester

--- root/quizzes/tests.py
+++ root/quizzes/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Category, SubCategory
 
-# Create your tests here.
+
+class TestCategory(TestCase):
+
+    def setUp(self):
+        self.c1 = Category.objects.new_category(category='vodka')
+        self.sub1 = SubCategory.objects.create(sub_category='absolute', category=self.c1)
+
+    def test_categories(self):
+        self.assertEqual(self.c1.category, 'vodka')
+
+    def test_sub_categories(self):
+        self.assertEquals(self.sub1.category, self.c1)
+
+
