88cfb72d32c72273169fa24a4db29226fdead98e
Marius Hornslien <mariushornslien@dhcp-10-24-17-243.wlan.ntnu.no>
2017-03-13 11:22:49 +0100

    lagt til adminfunksjoner for quiz

--- root/quizzes/admin.py
+++ root/quizzes/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Question
+from .models import Category
+from .models import SubCategory
+from .models import Quiz
+
 
 admin.site.register(Question)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Quiz)
