cba8c384f4cccf206633211815063c5f4000e0f1
Marius Hornslien <mariushornslien@dhcp-10-24-16-97.wlan.ntnu.no>
2017-03-16 19:27:00 +0100

    endret timezone

--- root/quizzes/tests.py
+++ root/quizzes/tests.py
@@ -42,6 +42,31 @@ class TestQuiz(TestCase):
         self.assertEqual(self.quiz2.url, 'url-2')
         self.assertEqual(self.quiz3.url, 'test-url-3')
         self.assertEqual(self.quiz4.url, 't-q4')
+'''
+class TestQuestion(TestCase):
+    def setUp(self):
+        self.quiz1 = Quiz.objects.create(id=1,
+                                         title='test quiz 1',
+                                         description='d1',
+                                         url='url1')
+        self.c1 = Category.objects.new_category(category='testcategory')
+        self.sub1 = SubCategory.objects.create(sub_category='testsub', category=self.c1)
+        self.q1 = Question.objects.create(id=1,
+                                          quiz=self.quiz1,
+                                          category=self.c1,
+                                          sub_category=self.sub1,
+                                          #content='Dette er en test',
+                                          #explanation='Dette er forklaringen'
+                                          )
+
+    def test_create_question(self):
+        self.assertEqual(self.q1.quiz, self.quiz1)
+        self.assertEqual(self.q1.category, self.c1)
+        self.assertEqual(self.q1.sub_category, self.sub1)
+        #self.assertEqual(self.q1.content, 'Dette er en test')
+        #self.assertEqual(self.q1.explanation, 'Dette er forklaringen')
+
+'''
 
 
 
--- root/root/settings.py
+++ root/root/settings.py
@@ -39,7 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 
+]
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'quizzes' app. Add more apps behind quizzes.
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=quizzes',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
