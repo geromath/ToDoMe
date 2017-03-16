9f63638f306678b0bedbb0372374d88fe933bb4e
Marius Hornslien <mariushornslien@dhcp-10-24-25-70.wlan.ntnu.no>
2017-03-16 13:10:16 +0100

    lagt inn templates for quizzes

--- root/quizzes/views.py
+++ root/quizzes/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
+import random
+
+from django.contrib.auth.decorators import login_required, permission_required
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404, render
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView, ListView, TemplateView, FormView
+
+from .forms import QuestionForm
+from .models import Quiz, Category, Progress, Sitting, Question
+
+
+
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to the quiz page!")
+
+
+class QuizMarkerMixin(object):
+    @method_decorator(login_required)
+    @method_decorator(permission_required('quiz.view_sittings'))
+    def dispatch(self, *args, **kwargs):
+        return super(QuizMarkerMixin, self).dispatch(*args, **kwargs)
+
+class SittingFilterTitleMixin(object):
+    def get_queryset(self):
+        queryset = super(SittingFilterTitleMixin, self).get_queryset()
+        quiz_filter = self.request.GET.get('quiz_filter')
+        if quiz_filter:
+            queryset = queryset.filter(quiz__title__icontains=quiz_filter)
+
+        return queryset
+
+class QuizListView(ListView):
+    model = Quiz
+
+    def get_queryset(self):
+        queryset = super(QuizListView, self).get_queryset()
+        return queryset.filter(draft=False)
+
+
+class QuizDetailView(DetailView):
+    model = Quiz
+    slug_field = 'url'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.object.draft and not request.user.has_perm('quiz.change_quiz'):
+            raise PermissionDenied
+
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
