1195579c17aaf5dc4bc4aefeae94f54c206462de
Marius Hornslien <mariushornslien@dhcp-10-24-25-70.wlan.ntnu.no>
2017-03-16 13:24:31 +0100

    lagt inn urls

--- root/quizzes/urls.py
+++ root/quizzes/urls.py
@@ -1,8 +1,47 @@
 from django.conf.urls import url
 
-from . import views
+
+from .views import QuizListView, CategoriesListView,\
+    ViewQuizListByCategory, QuizUserProgressView, QuizMarkingList,\
+    QuizMarkingDetail, QuizDetailView, QuizTake
+
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+
+    # All of the below are subpages of /quizzes/ ?
+
+    url(regex=r'^$',
+        view=QuizListView.as_view(),
+        name='quiz_index'),
+
+    url(regex=r'^category/$',
+        view=CategoriesListView.as_view(),
+        name='quiz_category_list_all'),
+
+    url(regex=r'^category/(?P<category_name>[\w|\W-]+)/$',
+        view=ViewQuizListByCategory.as_view(),
+        name='quiz_category_list_matching'),
+
+    url(regex=r'^progress/$',
+        view=QuizUserProgressView.as_view(),
+        name='quiz_progress'),
+
+    url(regex=r'^marking/$',
+        view=QuizMarkingList.as_view(),
+        name='quiz_marking'),
+
+    url(regex=r'^marking/(?P<pk>[\d.]+)/$',
+        view=QuizMarkingDetail.as_view(),
+        name='quiz_marking_detail'),
+
+    #  passes variable 'quiz_name' to quiz_take view
+    url(regex=r'^(?P<slug>[\w-]+)/$',
+        view=QuizDetailView.as_view(),
+        name='quiz_start_page'),
+
+    url(regex=r'^(?P<quiz_name>[\w-]+)/take/$',
+        view=QuizTake.as_view(),
+        name='quiz_question'),
 ]
 
