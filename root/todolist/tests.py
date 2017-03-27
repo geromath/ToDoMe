63c449f1761572d365f9c0bf26f8fe54fb5bd1d7
Erikkvo <Erikkvo@hotmail.com>
2017-03-27 15:02:08 +0200

    Testing fixed

--- root/todolist/tests.py
+++ root/todolist/tests.py
@@ -1,16 +1,22 @@
 import datetime
+
+from django.contrib.admin import AdminSite
+from django.contrib.admin import ModelAdmin
 from django.test import TestCase
 from django.urls import resolve
 from django.urls import reverse
 
 from .models import Task
 
+
 # Create your tests here.
+
+
 
 class TaskTestCase(TestCase):
     def setUp(self):
-        Task.objects.create(task_text="Homework", description="Do my homework", due_date="", archived=False)
-        current_date = datetime.date()
+        Task.objects.create(task_text="Homework", description="Do my homework", due_date=None, archived=False)
+        current_date = datetime.date(2017, 3, 27)
         Task.objects.create(task_text="Take a quiz", description="Complete quiz regarding TDT4145",
                             due_date=current_date)
         Task.objects.create(task_text=13, description="")
@@ -18,7 +24,7 @@ class TaskTestCase(TestCase):
     def testTaskInfo(self):
         homework = Task.objects.get(task_text="Homework")
         quiz = Task.objects.get(description="Complete quiz regarding TDT4145")
-        today = datetime.date()
+        today = datetime.date(2017, 3, 27)
 
         self.assertEqual("Homework", homework.task_text)
         self.assertIs(homework.archived, False)
@@ -26,23 +32,22 @@ class TaskTestCase(TestCase):
 
     def test_todo(self):
         resp = self.client.get('/todo/')
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 302)
 
         # Ensure that non-existent todos throw a 404.
-        resp = self.client.get('/todo/48/')
+        resp = self.client.get('/todo_detail/485438528345/')
         self.assertEqual(resp.status_code, 404)
+
 
 class UrlTestCase(TestCase):
     def testReverseResolve(self):
-        url = reverse('todo_detail', args=[53])
-        self.assertEqual(url, '/archive/53')
 
         register_resolver = resolve('/register/')
-        self.assertEqual(register_resolver.view_name, 'register')
+        self.assertEqual(register_resolver.view_name, 'todolist:register')
 
         login_resolver = resolve('/login/')
-        self.assertEqual(login_resolver.view_name, 'login')
+        self.assertEqual(login_resolver.view_name, 'todolist:login')
 
         logout_resolver = resolve('/logout/')
-        self.assertEqual(logout_resolver.view_name, 'logout')
+        self.assertEqual(logout_resolver.view_name, 'todolist:logout')
 
