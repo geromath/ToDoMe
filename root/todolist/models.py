13cc8ed0940f3a8e517df27338d96ea049280b23
Erikkvo <Erikkvo@hotmail.com>
2017-04-07 14:45:10 +0200

    Main view changes

--- root/todolist/models.py
+++ root/todolist/models.py
@@ -3,16 +3,18 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.contrib import admin
 
-class TaskManager(models.Manager):
 
+class TaskManager(models.Manager):
     def active(self, *args, **kwargs):
         return super(TaskManager, self).filter(archived=False)
+
     def archived(self, *args, **kwargs):
         return super(TaskManager, self).filter(archived=True)
 
+
 class Task(models.Model):
-    task_text = models.CharField(max_length = 150)
-    description = models.TextField(max_length = 500)
+    task_text = models.CharField(max_length=150)
+    description = models.TextField(max_length=500)
     due_date = models.DateField(default=None, null=True, blank=True)
     archived = models.BooleanField(default=False)
     time_created = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -24,8 +26,6 @@ class Task(models.Model):
     def get_color(self):
         return '#808080' if not self.color else self.color
 
-
-
     def get_absolute_url(self):
         return reverse('todolist:index', kwargs={"id": self.id})
 
@@ -35,11 +35,14 @@ class Task(models.Model):
     class Meta:
         ordering = ["due_date", "-time_created"]
 
+
 class TaskAdmin(admin.ModelAdmin):
     fields = ['task_text', 'description', 'due_date']
+
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         super.save_model(request, obj, form, change)
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
