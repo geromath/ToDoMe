b8188835d948774db8e7dc2aa940b9277fd7915a
Erikkvo <Erikkvo@hotmail.com>
2017-04-24 12:31:46 +0200

    Facebook login fixed directly in master

--- root/root/settings.py
+++ root/root/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'todolist',
     'quizzes',
+    'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
